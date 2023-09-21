338a16cae2a831f3eb77cd6b75b6e555e149a40c
Jens-Jakob <KM44VW@student.aau.dk>
2023-09-21 11:51:47 +0200

    Kommentar til "Kingdomino.py"

--- kingdomino.py
+++ kingdomino.py
@@ -2,34 +2,49 @@ import cv2 as cv
 import numpy as np
 import os
 
-# Main function containing the backbone of the program
+# Definerer hovedfunktionen
 def main():
     print("+-------------------------------+")
     print("| King Domino points calculator |")
     print("+-------------------------------+")
-    image_path = r"/Users/jens-jakobskotingerslev/Documents/GitHub/P0/King Domino dataset (1)/1.jpg"
+
+    # Opretter en variabel, med stien til billede
+    image_path = r"/Users/jens-jakobskotingerslev/Documents/GitHub/P0/King Domino testset/60.jpg"
+    # Hvis billede ikke kan findes, vil funktionen printe "Image not found"
     if not os.path.isfile(image_path):
         print("Image not found")
         return
+    # Definerer "image" som en variable, der bruger cv funktionen cv.imread, til at aflæse billeder"
     image = cv.imread(image_path)
+    # Definerer "tiles" som henter "get_tiles" funktionen til "image" variablet
     tiles = get_tiles(image)
+    # Printer antal af "tiles" for "for loopet"
     print(len(tiles))
+    # "for loopet" ser og gentager hvert element på y-aksen i listen "tiles"
     for y, row in enumerate(tiles):
+    # "nested loopet" ser og gentager hvert element på x-aksen i listen "tiles"
         for x, tile in enumerate(row):
+    # "print" funktionen, printer så hvor hvert billede er, samt hvilket slags billede det er. 
             print(f"Tile ({x}, {y}):")
             print(get_terrain(tile))
             print("=====")
 
-# Break a board into tiles
+# deler billedet op i 25 dele
+# Definerer "get_tiles" som image variablet
 def get_tiles(image):
+    # laver en tom liste 
     tiles = []
+    # skaber et "for loop" hvor elementer vil blive tilføjet til listen, hvor y repræsenterer en række af billedet 
     for y in range(5):
         tiles.append([])
+    # Skaber et "nested loop, hvor billedet bliver delt op i en tavel med 25 kvadrater af 100,100 px og tilføjer til listen "tiles"
         for x in range(5):
             tiles[-1].append(image[y*100:(y+1)*100, x*100:(x+1)*100])
     return tiles
 
-# Determine the type of terrain in a tile
+# Bestemmer "tile" type
+# Definerer en funktion, der først konverterer "rgb" værdierne for farverine til "hsv"værdier
+# derefter bruger funktionen "NumPy" funktionen "mean" til at finde "hsv" gennemsnitsværiden for billede tabellen
 def get_terrain(tile):
     hsv_tile = cv.cvtColor(tile, cv.COLOR_BGR2HSV)
     hue, saturation, value = np.mean(hsv_tile, axis=(0, 1))
@@ -60,6 +75,7 @@ def get_terrain(tile):
 
     return "Unknown"
 
+# "main" funktionen kører hvis programmet navn er "__main__"
 if __name__ == "__main__":
     main()
 

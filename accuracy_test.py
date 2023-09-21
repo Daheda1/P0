477f6e365d51c7b0112838a81846bd1bf3012345
Daheda1 <danni.dahl@gmail.com>
2023-09-21 13:43:14 +0200

    nemmere kommentare

--- accuracy_test.py
+++ accuracy_test.py
@@ -1,4 +1,4 @@
-from kingdomino import get_terrain
+from kingdomino import get_terrain #Henter get_terrain
 import os
 import cv2 as cv
 import pandas as pd
@@ -33,19 +33,29 @@ def test_get_terrain(folder_path):
                 if tile is None:
                     print(f"Could not read the image from {tile_path}. Skipping...")
                     continue
-                # Vi definerer predicted_terrain ud fra get_terrain-funktionen, der aflæser et tile. 
+                # Vi definerer predicted_terrain ud fra get_terrain funktionen, der aflæser et tile. 
                 predicted_terrain = get_terrain(tile)
                 unique_terrains.add(predicted_terrain)
 
-                # Definerer confunsion_matrix??? 
-                confusion_matrix.at[terrain_type, predicted_terrain] = confusion_matrix.at[terrain_type, predicted_terrain] + 1 if terrain_type in confusion_matrix.index and predicted_terrain in confusion_matrix.columns else 1
+                # Hvis både 'terrain_type' og 'predicted_terrain' findes i DataFrame'ens index og kolonner,
+                # så inkrementeres cellens værdi med 1.
+                # Hvis ikke, så initialiseres cellen med værdien 1.
+                if terrain_type in confusion_matrix.index and predicted_terrain in confusion_matrix.columns:
+                    confusion_matrix.at[terrain_type, predicted_terrain] += 1
+                else:
+                    confusion_matrix.at[terrain_type, predicted_terrain] = 1
 
                 total_count += 1
                 total_counts[terrain_type] = total_counts.get(terrain_type, 0) + 1
-                # Tjekker om predicted_terrain er forskellig fra terrian_type. Herefter ligger den 1 til i total_correct
-                # linje 49 forstår jeg ikke helt
+                
+                # Tjekker om den forudsagte terræntype matcher den faktiske terræntype.
+                # Hvis de matcher, er klassificeringen korrekt.
                 if predicted_terrain == terrain_type:
+                    # Inkrementerer det samlede antal korrekte klassificeringer.
                     total_correct += 1
+                    
+                    # Opdaterer antallet af korrekte klassificeringer for den specifikke terræntype.
+                    # Hvis terræntypen ikke allerede findes i 'correct_counts', initialiseres den med 0 og derefter inkrementeres med 1.
                     correct_counts[terrain_type] = correct_counts.get(terrain_type, 0) + 1
     
     # Beregner en overall accuracy i procent og printer svaret som en message. 
@@ -60,8 +70,7 @@ def test_get_terrain(folder_path):
     # Fill missing values in confusion matrix with 0 and convert to integers
     confusion_matrix = confusion_matrix.fillna(0).astype(int)
     
-    return confusion_matrix
+    print (confusion_matrix)
 
-# Printer confusion_matrix ud fra funktionen test_get_terrain. 
-conf_matrix = test_get_terrain("/Users/dannihedegaarddahl/Documents/GitHub/P0/categories testset")
-print(conf_matrix)
+# Køre vores program på den mappe den er givet
+test_get_terrain("/Users/dannihedegaarddahl/Documents/GitHub/P0/categories testset")
